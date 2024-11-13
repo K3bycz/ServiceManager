86d7996334d9304119ed1702351e29a619adb172
K3bycz <pkapustka@edu.cdv.pl>
2024-11-13 12:07:54 +0100

    Bootstrap Instalation

---
+++ public/index.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+    </head>
+    <body>
+        <div class="container text-center">
+           Strona główna
+        </div>
+    </body>
+</html>
